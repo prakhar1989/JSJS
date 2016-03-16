7a381b780b162c898b2a723369fe177762b2dc70
Prakhar Srivastav <prakhar1989@gmail.com>
2016-03-15 21:58:58 -0400

    codegen for if-then-else and blocks works. #27

--- src/codegen.ml
+++ src/codegen.ml
@@ -1,11 +1,22 @@
 open Ast
 open Stringify
 
+let block_template exprs ret_expr =
+  let template = format_of_string "
+      (function() {
+        %s
+        return %s
+      })()"
+  in
+  Printf.sprintf template exprs ret_expr
+;;
+
 let rec js_of_expr = function
   | NumLit(x) -> Printf.sprintf "%s" (string_of_float x)
   | StrLit(s) -> Printf.sprintf "\"%s\"" s
   | Val(s) -> s
   | BoolLit(b) -> Printf.sprintf "%s" (string_of_bool b)
+  | UnitLit -> "null"
   | Unop(o, e) ->
     let s1 = string_of_op o and s2 = js_of_expr e in
     Printf.sprintf "%s%s" s1 s2
@@ -15,6 +26,28 @@ let rec js_of_expr = function
       | _ -> string_of_op o
     and s2 = js_of_expr e1 and s3 = js_of_expr e2 in
     Printf.sprintf "(%s %s %s)" s2 sop s3
-  | Assign(id, _, e) -> Printf.sprintf "let %s = %s;" id (js_of_expr e)
+  | Assign(id, _, e) -> Printf.sprintf "let %s = %s" id (js_of_expr e)
+  | Block(es) ->
+    let es = List.rev (List.map js_of_expr es) in
+    (match es with
+    | [] -> "" (* will never be reached *)
+    | x :: [] -> block_template "" x
+    | x :: xs ->
+      let es = String.concat "\n" (List.rev xs) in
+      block_template es x)
+  | If(p, e1, e2) ->
+    let pred_s = js_of_expr p
+    and s1 = js_of_expr e1 and s2 = js_of_expr e2
+    and template = format_of_string "(function() {
+        let %s
+        if (%s) {
+            %s = %s
+        } else {
+            %s = %s
+        }
+        return %s
+    })()" in
+    let name = "res_" ^ string_of_int(Random.int 1000000) in
+    Printf.sprintf template name pred_s name s1 name s2 name
   | _ -> ""
 ;;
