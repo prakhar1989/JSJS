b38a057a21468c758fb92748207257504dc30605
Bahul Jain <bkj2111@columbia.edu>
2016-04-08 01:01:58 +0000

    added List.insert, #32

--- src/lib.ml
+++ src/lib.ml
@@ -73,6 +73,7 @@ let list_definitions = [
   ("range", TFun([TNum; TNum], TList(TNum)));
   ("concat", TFunGeneric(([TList(T('T')); TList(T('T'))], TList(T('T'))), ['T']));
   ("reduce", TFunGeneric(([TFun([T('T'); T('U')], T('T')); T('T'); TList(T('U'))], T('T')), ['T'; 'U']));
+  ("insert", TFunGeneric( ([TList(T('T')); T('T'); TNum], TList(T('T'))), ['T']) );
 ];;
 
 let map_definitions = [
